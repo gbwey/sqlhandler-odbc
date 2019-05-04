05b03d497bbbb25bdfd6f9634f994ed48a232806
Your Name <you@example.com>
2019-05-04 12:19:44 -0400

    simplify sql

---
+++ mysql.sql
@@ -0,0 +1,14 @@
+create table mixed
+( id numeric(4,0) not null primary key
+ ,total numeric(8,2) not null
+ ,eventdate date not null
+ ,description varchar(100) not null
+);
+
+insert into mixed values(1, 123.76, '2010-01-02', 'first row');
+insert into mixed values(2, 212.04, '2005-03-04', 'second row');
+insert into mixed values(3, 0, '1990-10-12', 'third row');
+insert into mixed values(4, 10.34, '2007-01-02', 'fourth row');
+insert into mixed values(5, 20.56, '2012-01-02', 'fifth row');
+insert into mixed values(6, 777, '2005-01-02', 'sixth row');
+
